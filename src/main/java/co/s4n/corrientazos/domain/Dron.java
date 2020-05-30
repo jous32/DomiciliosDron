788821bd7c5dd6c8e8d6d14fb74f8702fc0557c4
josevasquez <jous32@gmailcom>
2020-05-30 11:09:37 -0500

    remove setters and add constructor method

--- src/main/java/co/s4n/corrientazos/domain/Dron.java
+++ src/main/java/co/s4n/corrientazos/domain/Dron.java
@@ -1,5 +1,40 @@
 package co.s4n.corrientazos.domain;
 
+import java.util.List;
+
+import co.s4n.corrientazos.domain.enums.DirectionEnum;
+
 public class Dron {
 
+	private String name;
+	private Position position;
+	private DirectionEnum directionEnum;
+	private List<Route> listRoutes;
+	
+	
+	public Dron(String name, Position position, DirectionEnum directionEnum, List<Route> listRoutes) {
+		this.name = name;
+		this.position = position;
+		this.directionEnum = directionEnum;
+		this.listRoutes = listRoutes;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public Position getPosition() {
+		return position;
+	}
+	
+	public DirectionEnum getDirectionEnum() {
+		return directionEnum;
+	}
+	
+	public List<Route> getListRoutes() {
+		return listRoutes;
+	}
+	
+	
+	
 }
